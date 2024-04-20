f18596df5e483701bd4ac4e5839dda4baa6eb1f8
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2024-04-20 16:11:08 +0800

    add water level pie chart, add color, add datamap

--- lib/src/waste_data/waste_data_view.dart
+++ lib/src/waste_data/waste_data_view.dart
@@ -4,6 +4,15 @@ import 'package:flutter_waste_app/src/common_widgets/custom_card.dart';
 import 'package:flutter_waste_app/src/settings/settings_view.dart';
 import 'package:flutter_waste_app/src/waste_data/modals/waste_data_check_modal.dart';
 import 'package:flutter_waste_app/src/waste_data/waste_data_function.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+Map<String, double> dataMap = {
+  "Water Level": 10,
+};
+
+List<Color> colorList = <Color>[
+  const Color.fromARGB(255, 201, 198, 64),
+];
 
 class WasteDataView extends StatelessWidget {
   const WasteDataView({
@@ -31,12 +40,25 @@ class WasteDataView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              width: 350,
+              height: 400,
+              child: PieChart(
+                dataMap: dataMap,
+                baseChartColor: const Color.fromARGB(255, 94, 88, 88),
+                colorList: colorList,
+                chartValuesOptions: const ChartValuesOptions(
+                  showChartValuesInPercentage: true,
+                ),
+                totalValue: 20,
+              ),
+            ),
             const Card(
-                child: CustomCard(childCard: Text("waste data : 40kg")),
+                child:
+                    CustomCard(childCard: Text("current water level : 40cm")),
                 color: Color.fromARGB(255, 11, 201, 201)),
             const Card(
-                child: CustomCard(
-                    childCard: Text("water sensing threshold : 0.8")),
+                child: CustomCard(childCard: Text("Max Level : 150cm")),
                 color: Color.fromARGB(255, 192, 149, 149)),
             Card(
               color: const Color.fromARGB(255, 77, 146, 59),
